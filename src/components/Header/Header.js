3f0f8b31ff6a967cf4512f19aaa9b4585a368f69
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-17 18:42:38 +0100

    Setup basic header and layout of the application

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img src='gmail_logo.png' alt='' />
+      </div>
+      <div className='header__middle'>
+        <SearchIcon />
+        <input placeholder='Search mail' type='text' />
+        <ArrowDropDownIcon className='heaer__inputCaret' />
+      </div>
+      <div className='header__right'>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
