7595368eff6984eb72fba6bcab98e88fef74d0df
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-19 00:10:59 +0100

    add EmailList, Mail, Section, Sidebar and SidebarOption components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Mail from './components/Mail/index';
+import EmailList from './components/EmailList';
 
 function App() {
   return (
-    <div className=''>
-      <Header />
-      <Sidebar />
-      {/* App body */}
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+          <Routes>
+            <Route path='/mail' element={<Mail />} />
+            <Route path='/' element={<EmailList />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
